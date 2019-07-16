a917cc0b99b7df51e6dbac567ba06532b7d103ea
punitda <punitdama@gmail.com>
2019-07-16 19:45:17 +0530

    Remove dep on electron-is-dev

--- src/main/electron.js
+++ src/main/electron.js
@@ -4,7 +4,7 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const isDev = require('electron-is-dev');
+const isDev = require('./electron-is-dev');
 
 let mainWindow;
 
