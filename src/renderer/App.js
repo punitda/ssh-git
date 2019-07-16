2f50c7af1aec817ef0d40c7c67540dc78cbdf5ce
punitda <punitdama@gmail.com>
2019-07-16 15:27:01 +0530

    Initial commit - make react work

---
+++ src/renderer/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is electron app to generate and manage ssh keys</h1>
+                <h2>copyright @2019 Punit Dama.</h2>
+            </div>
+        )
+    }
+}
+export default App;
