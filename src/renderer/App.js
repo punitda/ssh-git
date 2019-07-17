76ba1b581280520dec6cbaad2068282da9776dc4
punitda <punitdama@gmail.com>
2019-07-17 16:57:45 +0530

    Add Tailwindcss to the project and setup scripts for it

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -3,8 +3,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is an electron app to generate and manage ssh keys</h1>
-        <h2>copyright @2019 Punit Dama.</h2>
+        <h1 className="text-4xl font-bold text-center text-blue-500">
+          This is an electron app to generate and manage ssh keys
+        </h1>
+        <h2 className="text-left text-gray-800 px-8">
+          copyright @2019 Punit Dama.
+        </h2>
       </div>
     );
   }
