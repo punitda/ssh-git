d04293817b4c009eeb8ede0da5d73e5aeb6b9537
punitda <punitdama@gmail.com>
2019-07-17 16:04:57 +0530

    Add prettier and format source code

--- src/main/electron.js
+++ src/main/electron.js
@@ -9,29 +9,30 @@ const isDev = require('./electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width : 900,
-        height : 680
-    })
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+  });
 
-    mainWindow.loadURL(
-        isDev ? "http://localhost:3000"
-        : `file://${path.join(__dirname, '/../../build/index.html')}`
-    )
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '/../../build/index.html')}`
+  );
 
-    mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on('ready',createWindow);
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-app.on('activate',() => {
-    if(mainWindow === null){
-        createWindow();
-    }
-})
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>This is an electron app to generate and manage ssh keys</h1>
-                <h2>copyright @2019 Punit Dama.</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>This is an electron app to generate and manage ssh keys</h1>
+        <h2>copyright @2019 Punit Dama.</h2>
+      </div>
+    );
+  }
 }
 export default App;
