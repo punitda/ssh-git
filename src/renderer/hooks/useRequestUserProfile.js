d2740723db07b1e72cabd43adafa55fa98c71899
punitda <punitdama@gmail.com>
2019-08-07 13:05:33 +0530

    Custom Hook - useRequestUserProfile
    Add custom hook for fetching user profile in 2nd step using access_token obtained from 1st step.

---
+++ src/renderer/hooks/useRequestUserProfile.js
@@ -0,0 +1,97 @@
+import { useReducer, useEffect } from 'react';
+
+import { providers } from '../../lib/config';
+import {
+  requestGithubUserProfile,
+  requestBitbucketUserProfile,
+  requestGitlabUserProfile,
+} from '../service/api';
+
+function fetchReducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return { ...state, isLoading: true, isError: false };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+        isError: false,
+      };
+    case 'FETCH_ERROR':
+      return { ...state, isLoading: false, isError: true };
+    default:
+      throw new Error(`Invalid action.type: ${action.type}`);
+  }
+}
+
+export function useRequestUserProfile(selectedProvider, token) {
+  const [state, dispatch] = useReducer(fetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: null,
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+
+    async function fetchUserProfile() {
+      let profile;
+      dispatch({ type: 'FETCH_INIT' });
+      try {
+        switch (selectedProvider) {
+          case providers.GITHUB:
+            profile = await requestGithubUserProfile(token);
+            if (!didCancel) {
+              dispatch({
+                type: 'FETCH_SUCCESS',
+                payload: {
+                  email: profile.email,
+                  username: profile.login,
+                },
+              });
+            }
+            break;
+          case providers.BITBUCKET:
+            profile = await requestBitbucketUserProfile(token);
+            if (!didCancel) {
+              dispatch({
+                type: 'FETCH_SUCCESS',
+                payload: {
+                  email: profile.email,
+                  username: profile.username,
+                },
+              });
+            }
+            break;
+          case providers.GITLAB:
+            profile = await requestGitlabUserProfile(token);
+            if (!didCancel) {
+              dispatch({
+                type: 'FETCH_SUCCESS',
+                payload: {
+                  email: profile.email,
+                  username: profile.username,
+                },
+              });
+            }
+            break;
+          default:
+            throw new Error(`Unsupported Provider: ${selectedProvider}`);
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      }
+    }
+
+    fetchUserProfile();
+
+    return () => {
+      didCancel = true;
+    };
+  }, [selectedProvider, token]);
+
+  return [state];
+}
--- src/renderer/pages/Auth.js
+++ src/renderer/pages/Auth.js
@@ -1,95 +1,48 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
 import { ClientStateContext } from '../Context';
-import {
-  requestGithubUserProfile,
-  requestBitbucketUserProfile,
-  requestGitlabUserProfile,
-} from '../service/api';
-import { providers } from '../../lib/config';
+import { useRequestUserProfile } from '../hooks/useRequestUserProfile';
 
 const Auth = () => {
   const clientStateContext = useContext(ClientStateContext);
   const {
     token = null,
-    username = null,
-    email = null,
     selectedProvider = null,
   } = clientStateContext.authState;
-  const [loading, setLoading] = useState(false);
 
-  async function fetchGithubUserProfile() {
-    setLoading(true);
-    const profile = await requestGithubUserProfile(token);
-    if (profile) {
-      clientStateContext.setAuthState({
-        email: profile.email,
-        username: profile.login,
-      });
-    }
-    setLoading(false);
-  }
+  const [{ data, isLoading, isError }] = useRequestUserProfile(
+    selectedProvider,
+    token
+  );
 
-  async function fetchBitbucketUserProfile() {
-    setLoading(true);
-    const profile = await requestBitbucketUserProfile(token);
-    if (profile) {
-      clientStateContext.setAuthState({
-        email: profile.email,
-        username: profile.username,
-      });
-    }
-    setLoading(false);
-  }
-
-  async function fetchGitlabUserProfile() {
-    setLoading(true);
-    const profile = await requestGitlabUserProfile(token);
-    if (profile) {
-      clientStateContext.setAuthState({
-        email: profile.email,
-        username: profile.username,
-      });
-    }
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    switch (selectedProvider) {
-      case providers.GITHUB:
-        fetchGithubUserProfile();
-        break;
-      case providers.BITBUCKET:
-        fetchBitbucketUserProfile();
-        break;
-      case providers.GITLAB:
-        fetchGitlabUserProfile();
-        break;
-      default:
-        console.error('Provider cannot be empty');
-        break;
-    }
-  }, []);
-
-  if (loading) {
+  if (isLoading) {
     return (
       <h1 className="flex flex-col items-center justify-center">Loading...</h1>
     );
+  }
+
+  if (isError) {
+    return (
+      <h1 className="flex flex-col items-center justify-center">
+        Something went wrong. Please try again.
+      </h1>
+    );
+  }
+
+  if (data) {
+    const { username, email } = data;
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <h1>User Name : {username}</h1>
+        <h1>Email : {email}</h1>
+      </div>
+    );
   } else {
-    if (username && email) {
-      return (
-        <div>
-          <h1>User Name : {username}</h1>
-          <h1>Email : {email}</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>Profile not found.</h1>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <h1>Profile not found.</h1>
+      </div>
+    );
   }
 };
 
