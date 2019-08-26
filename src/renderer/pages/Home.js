940803e7140434a73d595717d3f21b8508c468d1
punitda <punitdama@gmail.com>
2019-08-26 12:17:33 +0530

    UI : Homescreen changes
    Change most of styling like text-size, button colors and margin, etc.

--- src/renderer/pages/Home.js
+++ src/renderer/pages/Home.js
@@ -14,16 +14,16 @@ export default function Home({ navigateTo }) {
         </a>
       </div>
       <div className="flex flex-col items-center pb-8">
-        <h1 className="mt-4 font-bold text-4xl text-white uppercase tracking-wider">
+        <h1 className="mt-4 font-bold text-5xl text-gray-200 uppercase tracking-wider">
           ssh-git
         </h1>
         <button
-          className="w-56 px-4 py-2 mt-20 text-white text-xl font-semibold bg-purple-700 hover:bg-purple-500 rounded focus:outline-none"
+          className="w-64 px-4 py-2 mt-24 text-white text-2xl font-semibold bg-blue-700 hover:bg-blue-500 rounded focus:outline-none"
           onClick={_e => navigateTo('/oauth')}>
           Setup SSH
         </button>
         <button
-          className="w-56 px-4 py-2 mt-8 text-gray-800 text-xl font-semibold bg-teal-300 hover:bg-teal-100 rounded focus:outline-none"
+          className="w-64 px-4 py-2 mt-8 text-gray-800 text-2xl font-semibold bg-orange-400 hover:bg-orange-200 rounded focus:outline-none"
           onClick={_e => navigateTo('/updateRemote')}>
           Update Remote
         </button>
