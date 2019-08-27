ae15645df6ef5930cfac3bafa76a47a46548190a
punitda <punitdama@gmail.com>
2019-08-27 19:28:08 +0530

    Second version of AddKeys screen

--- src/renderer/pages/sshsetup/AddKeys.js
+++ src/renderer/pages/sshsetup/AddKeys.js
@@ -14,32 +14,37 @@ function AddKeys({ onNext }) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mx-auto">
-      <h2 className="mx-16 mt-8 mb-8 text-xl text-center text-gray-900">
-        To start using the ssh key that was generated in last step, you need to
-        add it to your account.
+    <div>
+      <h2 className="mx-16 mt-8 text-2xl text-center text-gray-900">
+        To start using the generated ssh key, you need to add it to your
+        account.
       </h2>
-      <button className="primary-btn">Add Keys</button>
-
-      <div className="flex flex-row mt-8 w-screen">
-        <span className="flex-1 m-auto border-b-2 border-dashed border-gray-500">
-          {''}
-        </span>
-        <span className="flex-none px-2 text-gray-700 text-xl">OR</span>
-        <span className="flex-1 m-auto border-b-2 border-dashed border-gray-500">
-          {''}
-        </span>
+      <p className="text-gray-600 text-sm text-center mx-8">
+        [Note : You can add keys either automatically(requires permissions){' '}
+        <span className="underline italic text-gray-700">OR</span> manually by
+        following the steps listed on right side]
+      </p>
+      <div className="flex flex-row mt-8 items-center justify-around ">
+        <h2 className="underline italic text-xl text-gray-700">
+          Automatic Process
+        </h2>
+        <h2 className="underline italic text-xl text-gray-700">
+          Manual Process
+        </h2>
       </div>
-      <h2 className="mx-16 mt-4 mb-4 text-xl text-center text-gray-900">
-        To add keys manually follow below steps
-      </h2>
-      <div className="flex flex-row m-2 justify-between items-start">
-        <div className="flex flex-col w-1/2 ml-4">
-          <h3 className="text-base font-semibold mt-2">Follow below steps:</h3>
-          <ul className="pr-12 text-left">
-            <li>
-              1. Copy Public key you seen on your right side to your clipboard.
-            </li>
+      <div className="flex flex-row mt-4 items-center justify-center">
+        <div className="flex flex-col flex-1">
+          <button className="primary-btn mx-auto">Add Keys</button>
+        </div>
+        <div className="flex flex-col flex-none h-56">
+          <span className="h-24 border-r-2 border-dashed border-gray-500 w-1"></span>
+          <span className="text-gray-700 py-2 -mx-3 text-xl">OR</span>
+          <span className="h-24 border-r-2 border-dashed border-gray-500 w-1"></span>
+        </div>
+        <div className="flex flex-col flex-1">
+          <h3 className="text-lg mt-2 ml-32">Steps:</h3>
+          <ul className="text-gray-700 pr-12 text-left ml-32 mt-2">
+            <li>1. Copy Public key you see below to your clipboard.</li>
             <li>2. Login into your Github account</li>
             <li>
               3. Go to this{' '}
@@ -52,24 +57,23 @@ function AddKeys({ onNext }) {
             <li>
               4. Click "New SSH key" button and paste the content copied on your
               clipboard under "key" input and for "title" input give any name to
-              identify the machine in future. <br />
-              [E.x: Macbook Pro(Office)].
+              identify the machine in future. [E.x: Macbook Pro(Office)].
             </li>
           </ul>
         </div>
-        <div className="flex flex-col w-1/2">
-          <h3 className="text-base font-semibold">Public Key:</h3>
-          <div className="relative">
-            <button className="absolute mt-2 py-2 px-4 font-semibold right-0 top-0 bg-green-500 text-white rounded">
-              Copy
-            </button>
-            <textarea
-              rows="8"
-              cols="5"
-              value={publicKeyContent}
-              className="text-gray-600 text-base bg-gray-200 px-4 rounded border-2 mt-2 w-full resize-none"
-            />
-          </div>
+      </div>
+      <div className="mx-auto my-4 w-1/2">
+        <h3 className="text-xl">Public Key</h3>
+        <div className="relative">
+          <button className="absolute mt-2 py-2 px-4 font-semibold right-0 top-0 bg-green-500 text-white rounded">
+            Copy
+          </button>
+          <textarea
+            rows="8"
+            cols="5"
+            value={publicKeyContent}
+            className="text-gray-600 text-base bg-gray-200 px-4 rounded border-2 mt-2 w-full resize-none"
+          />
         </div>
       </div>
     </div>
