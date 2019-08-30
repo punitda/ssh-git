240accdbb461a3cef17476fc4b2e23ce0e5ef8a5
punitda <punitdama@gmail.com>
2019-08-30 20:48:56 +0530

    Conditional rendering in updateRemote screen
    This is because updateRemote will be used from 2 different flows - direct and stepped so we've to conditionally render toolbar and navbar based on props isDirect.

--- src/renderer/pages/updateremote/UpdateRemote.js
+++ src/renderer/pages/updateremote/UpdateRemote.js
@@ -3,14 +3,18 @@ import React from 'react';
 import Toolbar from '../../components/Toolbar';
 import { history } from '../../App';
 
-export default function UpdateRemote() {
+export default function UpdateRemote({ isDirect = false }) {
   function goBackToHomeScreen() {
     history.navigate('');
   }
 
-  return (
-    <div className="bg-gray-300 h-screen">
-      <Toolbar onBackPressed={goBackToHomeScreen} title="Update Remote" />
-    </div>
-  );
+  if (isDirect) {
+    return (
+      <div className="bg-gray-300 h-screen">
+        <Toolbar onBackPressed={goBackToHomeScreen} title="Update Remote" />
+      </div>
+    );
+  } else {
+    return <h1 className="text-center text-2xl">This is fine</h1>;
+  }
 }
