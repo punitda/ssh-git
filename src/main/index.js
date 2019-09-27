cb1136b302cde97e37b71e6fb7e7855290c70a52
punitda <punitdama@gmail.com>
2019-09-27 16:43:21 +0530

    handling open-url events when second-instance event is fired
    Trying this for Linux and Windows and expecting it to work. Haha!

--- src/main/index.js
+++ src/main/index.js
@@ -7,6 +7,20 @@ const ipc = require('./ipc');
 function init() {
   app.setName('ssh-git');
 
+  const gotTheLock = app.requestSingleInstanceLock();
+  if (!gotTheLock) {
+    app.quit();
+    return;
+  }
+
+  app.on('second-instance', (event, argv, _workingDir) => {
+    const mainWindow = window.getMainWindow();
+    if (mainWindow) {
+      helpers.showWindow(mainWindow);
+    }
+    app.emit('open-url', event, argv.pop() || '');
+  });
+
   app.on('ready', () => {
     helpers.registerAppSchema();
     window.createWindow();
@@ -19,12 +33,17 @@ function init() {
     }
   });
 
-  app.on('activate', () => {
-    if (window.getMainWindow() === null) {
-      window.createWindow();
+  app.on('activate', (_event, hasVisibleWindows) => {
+    if (!hasVisibleWindows) {
+      try {
+        window.createWindow();
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 
+  // MacOS only(for handling deeplinks)
   app.on('open-url', (event, url) => {
     event.preventDefault();
     helpers.handleAppURL(url, window.getMainWindow(), ipc.getGithubConfig());
