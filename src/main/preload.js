288e664e9a965f37c6cd820a7981409702fc1a42
punitda <punitdama@gmail.com>
2019-10-28 21:12:53 +0530

    Add ipcRenderer from electron-better-ipc on global window object for renderer

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,1 +1,2 @@
-window.ipcRenderer = require('electron').ipcRenderer;
+window.ipc = require('electron-better-ipc').ipcRenderer; // Using electron-better-ipc for better ipc comm api.
+window.ipcRenderer = require('electron').ipcRenderer; // Using default ipc api in some places
