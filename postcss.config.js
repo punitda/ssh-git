6312faba1f7eb344344e52867711f49bf5b28dc8
punitd <punitdama@gmail.com>
2019-11-11 12:35:09 +0530

    Purge css using postcss prefixer (#27)
    Though it is not that important considering we ship entire css packaged into app anyway unlike website and few 100kbs shouldn't matter. However, i think considering it doesn't take that much effort it is worth doing it in IMO.
    Without purging final tailwind.css file size : 510Kb
    With purging final tailwind.css file size : 7 Kb  🤯

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' &&
+      require('@fullhuman/postcss-purgecss')({
+        content: ['./src/renderer/**/*.js', './public/index.html'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
 };
