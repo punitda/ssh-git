60ab694780f836054a734261115642cb7be3d238
punitda <punitdama@gmail.com>
2019-11-28 17:31:55 +0530

    Add Sentry for crash and error reporting

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,1 +1,5 @@
 window.ipc = require('electron-better-ipc').ipcRenderer; // Using electron-better-ipc for better ipc comm api.
+
+// Init sentry for renderer process
+const initSentry = require('../lib/crash-reporter');
+initSentry();
