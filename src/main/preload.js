46d9b132e0838a1ad7eac6df265508cc8453857a
PunitD <punitdama@gmail.com>
2019-11-29 13:27:23 +0530

    Add global error handler using electron-unhandled (#31)

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,5 +1,6 @@
 window.ipc = require('electron-better-ipc').ipcRenderer; // Using electron-better-ipc for better ipc comm api.
 
-// Init sentry for renderer process
-const initSentry = require('../lib/crash-reporter');
+// Init sentry for renderer process and catch global errors on window
+const { initSentry, catchGlobalErrors } = require('../lib/crash-reporter');
 initSentry();
+catchGlobalErrors();
