66b716f5eceee3c36529146bcaa382b9dd73d0d2
punitda <punitdama@gmail.com>
2020-01-06 15:36:52 +0530

    Landing page UI changes
    Added react-typical for typing animations.
    Changed step cards text colors and background.

--- src/renderer/pages/Home.js
+++ src/renderer/pages/Home.js
@@ -5,6 +5,7 @@ import logo from '../../assets/logo/icon.png';
 
 import { Reveal, RevealGlobalStyles, Animation } from 'react-genie';
 import { trackScreen, trackEvent } from '../analytics';
+import Typical from 'react-typical';
 
 function renderLandingPage(navigateTo) {
   React.useEffect(() => {
@@ -22,26 +23,45 @@ function renderLandingPage(navigateTo) {
           <img src={logo} className="w-20 h-20" />
         </Reveal>
         <Reveal delay={200}>
-          <h1 className="mt-2 font-semibold text-4xl text-gray-200 tracking-wide">
-            Welcome to ssh-git
+          <h1 className="mt-2 font-semibold text-4xl mx-auto text-center text-gray-200">
+            Painlessly manage ssh keys for{' '}
           </h1>
         </Reveal>
+        <Reveal delay={500}>
+          <Typical
+            steps={[
+              'Github',
+              2000,
+              'Bitbucket',
+              1000,
+              'Gitlab',
+              1000,
+              '',
+              1000,
+              'Github, Bitbucket and Gitlab accounts',
+              1000,
+            ]}
+            loop={1}
+            wrapper="span"
+            className="font-semibold text-4xl mx-auto text-center text-gray-200"
+          />
+        </Reveal>
         <Reveal delay={300}>
-          <h2 className="text-gray-200 text-2xl text-center tracking-wide">
-            Painlessly manage ssh keys for Github, Bitbucket and Gitlab accounts
+          <h2 className="text-gray-200 text-lg mt-2 text-center">
+            Setup SSH keys in matter of secs 🚀
           </h2>
         </Reveal>
-        <div className="mt-16 flex flex-row justify-between items-center ">
+        <div className="mt-12 flex flex-row justify-between items-center ">
           {steps.map((step, index) => (
             <Reveal delay={400 + (index + 1) * 100}>
-              <div className="w-48 h-48 mr-8 flex flex-col items-center bg-gray-700 rounded-lg shadow-lg">
+              <div className="w-48 h-48 mr-8 flex flex-col items-center border-blue-500 border-r-2 border-b-2 rounded-lg shadow-2xl bg-gray-900">
                 <div className="w-8 h-8 mt-4 text-gray-700 text-xl text-center font-semibold bg-gray-100 rounded-full shadow">
                   {index + 1}
                 </div>
-                <h1 className="mt-2 px-4 text-xl text-center font-bold text-gray-300">
+                <h1 className="mt-4 px-4 text-2xl text-center font-bold text-gray-400">
                   {step.title}
                 </h1>
-                <h2 className="mt-4 px-4 text-center text-sm font-semibold text-gray-400">
+                <h2 className="mt-2 px-4 text-center text-xs font-semibold text-gray-400">
                   {step.content}
                 </h2>
               </div>
@@ -109,11 +129,11 @@ export default function Home({ navigateTo }) {
 const steps = [
   {
     title: 'Connect',
-    content: 'Connect to your account to get basic info',
+    content: 'Connect to your account',
   },
   {
     title: 'Generate',
-    content: 'Add passphrase to protect and generate SSH key',
+    content: 'Generate passphrase protected SSH key',
   },
   {
     title: 'Add',
@@ -121,6 +141,6 @@ const steps = [
   },
   {
     title: 'Clone',
-    content: 'You can now clone your repos without typing any passwords!',
+    content: 'You can now clone, push, pull your repo',
   },
 ];
