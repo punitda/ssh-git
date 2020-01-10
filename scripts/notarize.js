816cc4b292319e9e4b5b903d42dc3a851d3301b8
PunitD <punitdama@gmail.com>
2020-01-10 19:36:16 +0530

    Chore : Update CI (#58)

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,26 +1,39 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const { notarize } = require('electron-notarize');
 
 module.exports = async function notarizing(context) {
-  console.log('starting with notarizing process...');
-  const { electronPlatformName, appOutDir } = context;
+  console.log('Starting with notarizing process...');
 
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
 
-  const appName = context.packager.appInfo.productFilename;
   if (!(process.env.APPLEID && process.env.APPLEIDPASS)) {
-    console.warn(
-      'Skipping MacOS notarization because required creds not provided'
-    );
+    console.warn('Skipping MacOS notarization...');
     return;
   }
 
-  return await notarize({
-    appBundleId: 'app.ssh-git',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+  const appName = `${context.packager.appInfo.productFilename}.app`;
+  const appPath = path.join(appOutDir, appName);
+
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+
+  try {
+    const notarizeConfig = {
+      appBundleId: 'app.ssh-git',
+      appPath,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
+    };
+    await notarize(notarizeConfig);
+  } catch (error) {
+    console.error(`Error notarizing: ${error.message}`);
+  }
+
+  console.log('Done notarizing!');
 };
