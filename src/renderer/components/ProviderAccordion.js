79bea090a575dd318d59bb329e8d935f8b5bc9e8
punitda <punitdama@gmail.com>
2020-02-29 17:17:19 +0530

    Remove outline on focus on primary action in cards

--- src/renderer/components/ProviderAccordion.js
+++ src/renderer/components/ProviderAccordion.js
@@ -119,13 +119,13 @@ const ProviderAccordion = ({ keys, onNewSshKeyClicked, onActionClicked }) => {
                   <div className="absolute bottom-0 w-full">
                     {key.mode === 'MULTI' ? (
                       <button
-                        className="bg-gray-300 hover:bg-gray-400 w-full h-12 text-gray-700 hover:text-gray-800 text-sm rounded-b-lg rounded-t-none"
+                        className="bg-gray-300 hover:bg-gray-400 w-full h-12 text-gray-700 hover:text-gray-800 text-sm rounded-b-lg rounded-t-none focus:outline-none"
                         onClick={() => onActionClicked('UPDATE_REMOTE', key)}>
                         Update Remote
                       </button>
                     ) : (
                       <button
-                        className="bg-gray-300 hover:bg-gray-400 w-full h-12 text-gray-700 hover:text-gray-800 text-sm rounded-b-lg rounded-t-none"
+                        className="bg-gray-300 hover:bg-gray-400 w-full h-12 text-gray-700 hover:text-gray-800 text-sm rounded-b-lg rounded-t-none focus:outline-none"
                         onClick={() => onActionClicked('CLONE_REPO', key)}>
                         Clone Repo
                       </button>
