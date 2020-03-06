9af52770864950130e1240316ffb01a89ceae94a
punitda <punitdama@gmail.com>
2020-03-06 13:01:01 +0530

    Expose clipboard from electron on window object

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,4 +1,5 @@
 window.ipc = require('electron-better-ipc').ipcRenderer; // Using electron-better-ipc for better ipc comm api.
+window.clipboard = require('electron').clipboard;
 
 // Init sentry for renderer process and catch global errors on window
 const { initSentry, catchGlobalErrors } = require('../lib/crash-reporter');
